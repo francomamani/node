739c221f459d9210549a68a7815f44819122c74a
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2013-09-02 20:10:16 -0400

    Ejemplo de uso de urls

--- ejemplo2.js
+++ ejemplo2.js
@@ -1,7 +1,26 @@
 var express = require('express');
 var app = express();
 
-app.get("/family/:mame?", function (request, response, next) {
-	
-
+app.get("/family/:name?", function (request, response, next) {
+	var name = request.params.name;
+	switch(name)
+	{
+		case "franco":
+		case "julia":
+		case "fernanda":
+			response.send("Welcome "+name);
+		break;
+		default:
+			next();
+	}	
 });
+app.get("/family/*?", function (request, response) {
+	response.send("the member family isn't listed");	
+});
+app.get("/?", function (request, response)
+{
+	response.send("Hey Guy! What's up!");
+});
+
+app.listen(3000);
+console.log("Welcome to exercise #2 of node.js\nPress Ctrl+C for exit");
